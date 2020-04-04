bb50c0a1b3f37caf5adc2431dcc633a41246f5d1
lolsson <luca.sol91@hotmail.com>
2020-04-04 23:35:26 +0200

    Fixar efter merge

--- HackTheCrisis/Models/Demand.cs
+++ HackTheCrisis/Models/Demand.cs
@@ -9,10 +9,16 @@ namespace HackTheCrisis.Models
 {
     public class Demand
     {
+        [Key]
+        public int DemandID { get; set; }
+        [Required]
         public string Item { get; set; }
         [Range(0, 10000, ErrorMessage = "Du har angiviet ett högt värde")]
         public int Quantity { get; set; }
         public DateTime WhenDoINeedIt { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
         public HealthCareUnit HealthCareUnit { get; set; }
     }
 }
